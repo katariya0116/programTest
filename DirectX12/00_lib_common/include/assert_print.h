042610edcab744d8ae4c94c3e3465977182a4a1b
katariya <katariya0116@gmail.com>
2018-07-29 17:27:40 +0900

    lib_commonプロジェクトの追加

---
+++ DirectX12/00_lib_common/include/assert_print.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "type.h"
+
+template<typename ... ARGS>
+static void Print(const c8* _fmt, ARGS const& ...);
+
+template<typename ... ARGS>
+static void PrintError(const c8* _fmt, ARGS const& ...);
+
+template<typename ... ARGS>
+static void PrintWarning(const c8* _fmt, ARGS const& ...);
+
+
+#ifdef _DEBUG
+
+#define PRINT(format, ...)			Print(format, __VA_ARGS__)
+#define PRINT_ERROR(format, ...)	PrintError(format, __VA_ARGS__)
+#define PRINT_WARNING(format, ...)	PrintWarning(format, __VA_ARGS__)
+
+#define Assert(x) assert(x)
+
+#else
+#define PRINT(format, ...)			
+#define PRINT_ERROR(format, ...)	
+#define PRINT_WARNING(format, ...)	
+
+#define Assert(x) 
+
+#endif
+
---
+++ DirectX12/00_lib_common/include/type.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef __TYPE_H__
+#define __TYPE_H__
+
+typedef bool				b8;
+typedef char				c8;
+typedef unsigned char		u8;
+typedef	short				s16;
+typedef unsigned short		u16;
+typedef int					s32;
+typedef unsigned int		u32;
+typedef long long			s64;
+typedef unsigned long long	u64;
+typedef float				f32;
+typedef double				f64;
+
+#define RCast reinterpret_cast
+#define SCast static_cast
+#define DCast dynamic_cast
+#define CCast const_cast
+
+#endif
